5c09ad5b8451c996e70111eef40a6a23698a751a
weiwei <weiwei@weiweideMacBook-Air.local>
2022-11-01 10:39:17 +0800

    modified some structure and style

--- src/pages/MoviePage.tsx
+++ src/pages/MoviePage.tsx
@@ -1,18 +1,20 @@
 import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 //useDispatch是要改變某個state時用到的,而useSelector是想要拿到某個資料時使用的
-import { setMovieTitle, setMovieId } from '../store/movieSlice'
+import { setMovies } from '../store/movieSlice'
 //這個setMovieTitle, setMovieId的"動作"是我們想要"dispatch"出去的
 import { RootState } from '..';
 import { useTranslation } from 'react-i18next'
+import { Movie } from '../store/movieSlice';
 
 function Movies(){
-    const movieTitles = useSelector((state:RootState)=>state.movies.title)
+    const movieList = useSelector((state:RootState)=>state.movies)
     //這邊的state會去看index中configuresotre的reducer們，選擇了movies這個reducer，再去movieSlicer選擇要用的key（也就是title）
     const [ movieName, setMovieName ] = useState("")
     const [ searchResultWords, setSearchResultWords] = useState("")
+    const [ nameSearchResult, setNameSearchResult ] = useState<Movie[]>([])
     const [ keyWord, setKeyWord] = useState("")
-    const [ keyWordResult, setKeyWordResult] = useState<string[]>([])
+    const [ keyWordResult, setKeyWordResult] = useState<Movie[]>([])
     //這邊的movieList使用到的state，等於是index中configuresotre的reducer，所以可以選要哪一個
     //選好後，會使用movieSlice的資料
     const dispatch = useDispatch()
@@ -28,16 +30,17 @@ function Movies(){
       fetch('https://json-faker.onrender.com/movies')
         .then(res => res.json())
         .then(data => {
-            dispatch(setMovieTitle(data.movies.map((movie: { title: string; }) => movie.title)))
-            dispatch(setMovieId(data.movies.map((movie: { id: number; }) => movie.id)))
+            const movies = data.movies as Movie[]
+            dispatch(setMovies(movies))
         });
       },[dispatch]);
 
     function searchMovie(){
       setMovieName("")
-      let searchedResults = movieTitles.filter((title)=> movieName === title)
+      let searchedResults = movieList.filter((movie)=> movieName === movie.title)
       if (searchedResults.length > 0){
-        setSearchResultWords(`${t('searchMovieResult')}` + searchedResults.map((result)=> result))
+        setSearchResultWords(`${t('searchMovieResult')}`)
+        setNameSearchResult(searchedResults)
       } else if (searchedResults.length === 0){
         setSearchResultWords(`${t('searchMovieNoResult')}`)
       }
@@ -45,7 +48,7 @@ function Movies(){
 
     function searchKeyWord(){
       setKeyWord("")
-      let keyWordsearchResult = movieTitles.filter((title) => title.includes(keyWord))
+      let keyWordsearchResult = movieList.filter((movie) => movie.title.includes(keyWord))
       if(keyWordsearchResult.length > 0){
         setKeyWordResult(keyWordsearchResult)
       }
@@ -67,7 +70,20 @@ function Movies(){
             <input className='search-input' value={movieName} onChange={(e)=>{setMovieName(e.target.value)}} placeholder={t('searchMoviePlaceholder')} />
             <button className='search-button' type='button' onClick={()=>{searchMovie()}}>{t('search')}</button>
           </div>
-        <div className='result-display'>{searchResultWords === "" ? "": searchResultWords }</div>
+        <div>
+        <div className='result-display'>
+        <div>{searchResultWords === "" ? "" : searchResultWords}</div>
+        { nameSearchResult.length > 0 ?
+        <div className='image-box'>
+              <img className='keyword-result-poster' src = {nameSearchResult[0]?.poster} alt="movie" />
+              <div className='info-area'>
+              <div className='keyword-result-title'  >{nameSearchResult[0]?.title}</div>
+              <div className='keyword-result-director' >{nameSearchResult[0]?.director}</div>
+              </div>
+        </div>
+        :""}
+        </div>
+        </div>
         </div>
         <div className='middle-line'></div>
         <div className='sub-area'>
@@ -78,13 +94,13 @@ function Movies(){
           </div>
           {keyWordResult.length === 0 ? 
           <div className='keyword-result-display'>
-          {movieTitles?.map((title)=>(
-              <div className='keyword-result' key = {title}>{title}</div>
+          {movieList?.map((movie)=>(
+                <div className='keyword-result-title' key = {movie.title}>{movie.title}</div>
           ))}
           </div>:
           <div className='keyword-result-display'>
-          {keyWordResult?.map((title)=>(
-              <div className='keyword-result'>{title}</div>
+          {keyWordResult?.map((movie)=>(
+              <div className='keyword-result-title' key = {movie.title}>{movie.title}</div>
           ))}
           </div>
           }
@@ -93,4 +109,4 @@ function Movies(){
     )
 }
 
-export default Movies
+export default Movies;
--- src/store/movieSlice.tsx
+++ src/store/movieSlice.tsx
@@ -1,53 +1,48 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-export interface MovieState {
-    director: string[];
-    id: number[];
-    poster: string[];
-    rating: string[];
-    runtime: number[];
-    title: string[];
-    year: number[];
-}
-
-  const initialState : MovieState = {
-    director: [],
-    id: [],
-    poster: [],
-    rating: [],
-    runtime: [],
-    title: [],
-    year: []
-  } 
-
-// first try --> 可先忽略
 // export interface MovieState {
-//     value: Array<Type>
+//     director: string[];
+//     id: number[];
+//     poster: string[];
+//     rating: string[];
+//     runtime: number[];
+//     title: string[];
+//     year: number[];
 // }
-// interface Type{
-//     director: string;
-//     id:number;
-//     poster: string;
-//     rating: string;
-//     runtime: number;
-//     title: string;
-//     year: number;   
-//   }
+
+//   const initialState : MovieState = {
+//     director: [],
+//     id: [],
+//     poster: [],
+//     rating: [],
+//     runtime: [],
+//     title: [],
+//     year: []
+//   } 
+
+export interface Movie{
+    director: string;
+    id:number;
+    poster: string;
+    rating: string;
+    runtime: number;
+    title: string;
+    year: number;   
+  }
+
+const initialState = [] as Movie[]
 
 export const movieSlice = createSlice({
     name: "movies",
     initialState,
     reducers: {
-        setMovieTitle:(state: MovieState, action: PayloadAction<string[]>)=>{
-            state.title = action.payload
-        },
-        setMovieId:(state: MovieState, action: PayloadAction<number[]>)=>{
-            state.id = action.payload
+        setMovies:(state, action: PayloadAction<Movie[]>)=>{
+            return action.payload;
         },
     }
 })
 
-export const { setMovieTitle,  setMovieId } = movieSlice.actions
+export const { setMovies } = movieSlice.actions
 export default movieSlice.reducer
 
 // 整個movieSlice就是關於movie的資料的狀態，你在裡面去定義可以用哪些方法去操作這些資料，以及他們現在的狀態
